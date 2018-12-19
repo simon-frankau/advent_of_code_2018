c4dadd541fe9462f1e2977ce5315310e70e8ed45
Simon Frankau <sgf@arbitrary.name>
2018-12-19 21:48:54 +0000

    Complete day 1a

--- day1a/src/main.rs
+++ day1a/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).expect("Read error");
+    let parts = buffer.split('\n').filter(|s| s != &"").map(|s| s.trim().parse::<i32>().expect("Parse error"));
+
+    let mut running_sum = 0;
+    for part in parts {
+        running_sum += part;
+    }
+
+    println!("{}", running_sum);
 }
