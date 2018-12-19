a091fca3a22e8f9cdaebbf77304a2f06fd0a9b9d
Simon Frankau <sgf@arbitrary.name>
2018-12-19 21:57:52 +0000

    rustfmt'd day1a

--- day1a/src/main.rs
+++ day1a/src/main.rs
@@ -1,11 +1,14 @@
+use std::collections::HashSet;
 use std::io;
 use std::io::Read;
-use std::collections::HashSet;
 
 fn main() {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer).expect("Read error");
-    let parts = buffer.split('\n').filter(|s| s != &"").map(|s| s.trim().parse::<i32>().expect("Parse error"));
+    let parts = buffer
+        .split('\n')
+        .filter(|s| s != &"")
+        .map(|s| s.trim().parse::<i32>().expect("Parse error"));
 
     let mut seen = HashSet::new();
     let mut cycled = false;
