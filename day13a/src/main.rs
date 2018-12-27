ac5cd8d0c34a42c9fad6fc6efb5db354d22bf623
Simon Frankau <sgf@arbitrary.name>
2018-12-27 00:10:59 +0000

    Day13a now has movement (except for junctions)

--- day13a/src/main.rs
+++ day13a/src/main.rs
@@ -1,7 +1,8 @@
+use std::collections::HashSet;
 use std::io;
 use std::io::BufRead;
 
-#[derive(Debug)]
+#[derive(PartialEq, PartialOrd, Eq, Ord, Debug, Clone, Copy)]
 enum Facing {
     Right,
     Down,
@@ -13,10 +14,11 @@ const TURN_LEFT_NEXT: i32 = 0;
 const GO_STRAIGHT_NEXT: i32 = 1;
 const TURN_RIGHT_NEXT: i32 = 2;
 
-#[derive(Debug)]
+#[derive(PartialEq, PartialOrd, Eq, Ord, Debug)]
+// NB: Field ordering used for sorting into correct update order.
 struct Train {
-    x: usize,
     y: usize,
+    x: usize,
     facing: Facing,
     step: i32,
 }
@@ -34,16 +36,16 @@ fn extract_trains(grid: &mut Vec<Vec<char>>) -> Vec<Train> {
                  '/' => {}
                  '\\' => {}
                  'v' => {
-                     trains.push(Train { x: x, y: y, facing: Facing::Down, step: TURN_LEFT_NEXT});
+                     trains.push(Train { y: y, x: x, facing: Facing::Down, step: TURN_LEFT_NEXT});
                  }
                  '^' => {
-                     trains.push(Train { x: x, y: y, facing: Facing::Up, step: TURN_LEFT_NEXT});
+                     trains.push(Train { y: y, x: x, facing: Facing::Up, step: TURN_LEFT_NEXT});
                  }
                  '<' => {
-                     trains.push(Train { x: x, y: y, facing: Facing::Left, step: TURN_LEFT_NEXT});
+                     trains.push(Train { y: y, x: x, facing: Facing::Left, step: TURN_LEFT_NEXT});
                  }
                  '>' => {
-                     trains.push(Train { x: x, y: y, facing: Facing::Right, step: TURN_LEFT_NEXT});
+                     trains.push(Train { y: y, x: x, facing: Facing::Right, step: TURN_LEFT_NEXT});
                  }
                  _ => {
                      panic!("Unexpected input: {}", c);
@@ -75,6 +77,67 @@ fn print_track(grid: &Vec<Vec<char>>) {
     }
 }
 
+fn check_collisions(trains: &Vec<Train>, x: usize, y: usize) -> Result<(), String> {
+    // This is inefficient, but I think it's ok for a small number of trains.
+    for train in trains.iter() {
+        if train.x == x && train.y == y {
+
+        }
+    }
+    Ok(())
+}
+
+fn step_trains(grid: &Vec<Vec<char>>, trains: &mut Vec<Train>) -> Result<(), String> {
+    trains.sort();
+
+    let mut locs = trains.iter().map(|t| (t.x, t.y)).collect::<HashSet<_>>();
+
+    for train in trains.iter_mut() {
+        // Update position
+        {
+            let mut x = train.x;
+            let mut y = train.y;
+            match train.facing {
+                 Facing::Right => x += 1,
+                 Facing::Down => y += 1,
+                 Facing::Left => x -= 1,
+                 Facing::Up => y -= 1,
+            }
+            if locs.contains(&(x, y)) {
+                return Err(format!("Train collision at {}, {}", x, y));
+            }
+            locs.remove(&(train.x, train.y));
+            train.x = x;
+            train.y = y;
+            locs.insert((x, y));
+        }
+
+        match (grid[train.y][train.x], train.facing) {
+            (' ', _) => panic!("Fell off the rails: {:?}", train),
+            ('-', _) => {}
+            ('|', _) => {}
+
+            ('/', Facing::Left) => train.facing = Facing::Down,
+            ('/', Facing::Right) => train.facing = Facing::Up,
+            ('/', Facing::Up) => train.facing = Facing::Right,
+            ('/', Facing::Down) => train.facing = Facing::Left,
+
+            ('\\', Facing::Left) => train.facing = Facing::Up,
+            ('\\', Facing::Right) => train.facing = Facing::Down,
+            ('\\', Facing::Up) => train.facing = Facing::Left,
+            ('\\', Facing::Down) => train.facing = Facing::Right,
+
+            ('+', _) => panic!("TODO: Junction at {}, {}", train.x, train.y),
+
+            (c, _) => {
+                panic!("Unexpected state: {:?} on {}", train, c);
+            }
+         }
+    }
+
+    Ok(())
+}
+
 fn main() {
     let stdin = io::stdin();
     let mut grid: Vec<Vec<char>> = stdin
@@ -86,5 +149,9 @@ fn main() {
     let mut trains = extract_trains(&mut grid);
 
     print_track(&grid);
-    println!("{:?}", trains)
+    println!("{:?}", trains);
+    loop {
+        step_trains(&grid, &mut trains);
+        println!("{:?}", trains);
+    }
 }
