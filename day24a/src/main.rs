9bb68cc46dea8a6bb537b6bda6d86f5d88210ce9
Simon Frankau <sgf@arbitrary.name>
2019-02-05 22:46:28 +0000

    day24a: add the target-selection mechanism

--- day24a/src/main.rs
+++ day24a/src/main.rs
@@ -1,9 +1,10 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::io;
 use std::io::BufRead;
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Side {
     Immune,
     Infection,
@@ -23,6 +24,7 @@ impl FromStr for Side {
 
 #[derive(Debug)]
 struct Group {
+    id: u64,
     side: Side,
     unit_count: u64,
     hit_points: u64,
@@ -31,6 +33,19 @@ struct Group {
     attack_damage: u64,
     attack_type: String,
     initiative: u64,
+}
+
+impl Group {
+    fn attack_multiplier(self: &Group, target: &Group) -> i64 {
+        let t = &self.attack_type;
+        if target.immune_modifiers.contains(t) {
+            0
+        } else if target.weak_modifiers.contains(t) {
+            2
+        } else {
+            1
+        }
+    }
 }
 
 fn read_immune_weak(str: &str) -> (HashSet<String>, HashSet<String>) {
@@ -58,13 +73,14 @@ fn read_immune_weak(str: &str) -> (HashSet<String>, HashSet<String>) {
     return (immunes, weaks);
 }
 
-fn read_group(str: &str) -> Group {
+fn read_group(str: &str, id: u64) -> Group {
     // Normalise the separators, and split up.
     let parts = str.split(',').collect::<Vec<_>>();
         // .map(|s| s.trim().parse::<i32>().expect("Parse error"))
     assert_eq!(parts.len(), 7, "Wrong number of fields in: {}", str);
     let (immunes, weaks) = read_immune_weak(parts[3]);
     Group {
+        id: id,
         side: parts[0].parse().unwrap(),
         unit_count: parts[1].parse().unwrap(),
         hit_points: parts[2].parse().unwrap(),
@@ -76,15 +92,47 @@ fn read_group(str: &str) -> Group {
     }
 }
 
+// Return a number based on effective power, then initiative (assumes initiative < 100), to sort by.
+fn power_rank(group: &&Group) -> i64 {
+    0 - (group.unit_count * group.attack_damage * 100 + group.initiative) as i64
+}
+
+fn select_targets(groups: &[Group]) -> HashMap<u64, Option<u64>> {
+    // Sort groups by effective power
+    let mut group_refs = groups.iter().collect::<Vec<&Group>>();
+    group_refs.sort_by_key(power_rank);
+
+    // Targets that have been chosen already, and can't be re-selected.
+    let mut chosen: HashSet<u64> = HashSet::new();
+    group_refs.iter().map(|attacker| {
+        // Use rev so that we get the highest effective power in a
+        // draw (max_by_key returns the last of equals)
+        let target = groups.iter()
+            .rev()
+            .filter(|target| target.side != attacker.side && target.unit_count != 0 && !chosen.contains(&target.id))
+            .max_by_key(|target| attacker.attack_multiplier(target));
+        let target_id = match target {
+            Some(some_target) => {
+                chosen.insert(some_target.id);
+                Some(some_target.id)
+            }
+            None => None
+        };
+        (attacker.id, target_id)
+    }).collect()
+}
 
 
 fn main() {
     let stdin = io::stdin();
-    let groups: Vec<Group> = stdin
+    let mut groups: Vec<Group> = stdin
         .lock()
         .lines()
-        .map(|s| read_group(&s.expect("Read error")))
+        .zip(0..)
+        .map(|(s, i)| read_group(&s.expect("Read error"), i))
         .collect();
+
+    println!("{:?}", select_targets(&mut groups));
 
     for group in groups.iter() {
         println!("{:?}", group);
