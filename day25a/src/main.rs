00590836c0330d9d328e21df819949cc191d3d7e
Simon Frankau <sgf@arbitrary.name>
2019-02-07 10:58:41 +0000

    Completed day25a

--- day25a/src/main.rs
+++ day25a/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
 
@@ -34,6 +35,48 @@ fn read_point(str: &str) -> Point {
     }
 }
 
+struct UnionFind {
+    u: HashMap<usize, Option<usize>>,
+}
+
+impl UnionFind {
+    fn new() -> UnionFind {
+        UnionFind {
+            u: HashMap::new(),
+        }
+    }
+
+    fn insert(self: &mut UnionFind, i: usize) {
+        if self.u.insert(i, None) != None {
+            panic!("Set {} already inserted", i);
+        }
+    }
+
+    // Union the sets and return the current id of the unioned sets.
+    fn union(self: &mut UnionFind, i: usize, j: usize) -> usize{
+        if let Some(parent_i) = self.u.get(&i).unwrap().clone() {
+            let res = self.union(parent_i, j);
+            self.u.insert(i, Some(res));
+            res
+        } else if let Some(parent_j) = self.u.get(&j).unwrap().clone() {
+            let res = self.union(i, parent_j);
+            self.u.insert(j, Some(res));
+            res
+        } else {
+            // Both have no parent, union 'em.
+            if i != j {
+                self.u.insert(j, Some(i));
+            }
+            i
+        }
+    }
+
+    // Count parent-less things. This will be the number of distinct sets.
+    fn count(self: &UnionFind) -> usize {
+        self.u.values().filter(|x| x.is_none()).count()
+    }
+}
+
 fn main() {
     let stdin = io::stdin();
     let points: Vec<Point> = stdin
@@ -52,5 +95,12 @@ fn main() {
         }
     }
 
-    println!("{:?}", edges);
+    let mut uf = UnionFind::new();
+    for i in 0..points.len() {
+        uf.insert(i);
+    }
+    for (i, j) in edges.iter() {
+        uf.union(*i, *j);
+    }
+    println!("{}", uf.count());
 }
