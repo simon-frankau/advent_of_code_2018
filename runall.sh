31d04b64b9c1e96415d6b3e210831be2fb204a4e
Simon Frankau <sgf@arbitrary.name>
2019-02-10 14:45:00 +0000

    Add data for days 1-5, and a script to run those cases

---
+++ runall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cargo run --bin day1a < day1a/input/input.txt
+cargo run --bin day1b < day1a/input/input.txt
+cargo run --bin day2a < day2a/input/input.txt
+cargo run --bin day2b < day2a/input/input.txt
+cargo run --bin day3a < day3a/input/input.txt
+cargo run --bin day3b < day3a/input/input.txt
+cargo run --bin day4a < day4a/input/input.txt
+cargo run --bin day4b < day4a/input/input.txt
+cargo run --bin day5a < day5a/input/input.txt
+cargo run --bin day5b < day5a/input/input.txt
