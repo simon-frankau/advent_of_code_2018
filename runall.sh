c57f7ec7c4ac618e578b4c299b441494ffcccbc7
Simon Frankau <sgf@arbitrary.name>
2019-02-10 14:52:02 +0000

    Added inputs etc. for days 6-10.

--- runall.sh
+++ runall.sh
@@ -9,3 +9,11 @@ cargo run --bin day4a < day4a/input/input.txt
 cargo run --bin day4b < day4a/input/input.txt
 cargo run --bin day5a < day5a/input/input.txt
 cargo run --bin day5b < day5a/input/input.txt
+cargo run --bin day6a < day6a/input/input.txt
+cargo run --bin day6b < day6a/input/input.txt
+cargo run --bin day7a < day7a/input/input.txt
+cargo run --bin day7b < day7a/input/input.txt
+cargo run --bin day8a < day8a/input/input.txt
+cargo run --bin day8b < day8a/input/input.txt
+cargo run --bin day9
+cargo run --bin day10 < day10/input/input.txt
